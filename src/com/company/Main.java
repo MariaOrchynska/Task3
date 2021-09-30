8fff00143b61f8f75d24d79c49aa9730c3142ca0
maria <mariia.orchynska.fl.2019@lpnu.ua>
2021-09-30 23:28:55 +0300

    NameSorting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,7 +13,7 @@ public class Main {
         country.add(new Country("Britain", "Europe", "1900", "London", "200000"));
         country.add(new Country("Spain", "Europe", "399", "Madrid", "272722"));
         country.add(new Country("Mexico", "USA", "1200", "Mexico", "37474"));
-        country.add(new Country("Egypt", "Africa", "200", "Irusalim", "947474"));
+        country.add(new Country("Egypt ", "Africa", "200", "Irusalim", "947474"));
         Collections.sort(country, new Comparator<Country>() {
             @Override
             public int compare(Country o1, Country o2) {
